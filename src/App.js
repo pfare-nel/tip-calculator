61b0e5021110084862c235e76711f797cb25cd1e
Pfarelo Nelwamondo <Pfare5057@gmail.com>
2024-02-28 01:41:34 +0200

    tip calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [value, setValue] = useState(0);
+  const [friendValue, setFriendValue] = useState(0);
+
+  let tip;
+
+  function handleInput(b) {
+    setBill(Number(b.target.value));
+  }
+
+  function handleSelect(event) {
+    setValue(Number(event.target.value));
+  }
+
+  function handleFriendSelect(event) {
+    setFriendValue(Number(event.target.value));
+  }
+
+  function handleReset() {
+    setBill(0);
+    setValue(0);
+    setFriendValue(0);
+  }
+
+  tip = (value + friendValue) / 2;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill bill={bill} handleInput={handleInput} />
+      <MyTip
+        value={value}
+        handleSelect={handleSelect}
+        text="How did you like the service? "
+      />
+      <MyTip
+        value={friendValue}
+        handleSelect={handleFriendSelect}
+        text="How did your friend like the service? "
+      />
+
+      <Result bill={bill} tip={tip} />
+      <Button onReset={handleReset} />
     </div>
   );
 }
 
-export default App;
+function Bill({ handleInput, bill }) {
+  return (
+    <div>
+      <span>How much was the bill? </span>
+      <input type="text" value={bill} onChange={handleInput} />
+    </div>
+  );
+}
+
+function MyTip({ value, handleSelect, text }) {
+  return (
+    <div>
+      <span>{text}</span>
+      <select value={value} onChange={(e) => handleSelect(e)}>
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was good (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Result({ bill, tip }) {
+  let total = bill + tip;
+  return (
+    <div>
+      <h1>
+        You pay {total} (${bill} + ${tip} tip)
+      </h1>
+    </div>
+  );
+}
+
+function Button({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
